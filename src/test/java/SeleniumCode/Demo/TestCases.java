1b8de4a09208be6617fd50d694253e99222c77bb
dheer <dheer@DHEERAJ>
2021-07-28 15:50:04 +0530

    commit for testcases files

---
+++ src/test/java/SeleniumCode/Demo/TestCases.java
@@ -0,0 +1,19 @@
+package SeleniumCode.Demo;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+	
+	@Test
+	public void testcase1()
+	{
+		System.out.println("Good morning111");
+	}
+
+	@Test
+	public void testcase2()
+	{
+		System.out.println("Good evening111");
+	}
+
+}
---
+++ src/test/java/SeleniumCode/Demo/TestCases2.java
@@ -0,0 +1,19 @@
+package SeleniumCode.Demo;
+
+import org.testng.annotations.Test;
+
+public class TestCases2 {
+	
+	@Test
+	public void testcase1()
+	{
+		System.out.println("Good morning222");
+	}
+
+	@Test
+	public void testcase2()
+	{
+		System.out.println("Good evening222");
+	}
+
+}
